2987778b4b79cceea3df7a2ba8a5d1951a93c90b
Igor Xisto <igorxisto12@gmail.com>
2021-12-13 19:01:22 -0300

    a

---
+++ data.js
@@ -0,0 +1,21 @@
+export default [
+    
+    {
+        title:
+            'Meia noite e pouca',
+        artist: 'Lowz',
+        cover:'11.png',
+        file:'Lowz-Meia noite e pouca .mp3',
+    },
+
+    {
+        title:
+            'pixote Mix',
+        artist: 'pixote',
+        cover:'3.png',
+        file:'Pixote.mp3',
+    },
+    
+    
+];
+
